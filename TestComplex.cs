31cf53906fd71186f10c676007aef0feaa113b60
Willi Lazarov <lazarov.w@gmail.com>
2022-02-15 11:17:50 +0100

    Rename test method output

--- TestComplex.cs
+++ TestComplex.cs
@@ -15,9 +15,9 @@ namespace cv1
             Console.Write(testName + ": ");
 
             if (Math.Abs(c1.realPart - c2.realPart) < Epsilon && Math.Abs(c1.imaginaryPart - c2.imaginaryPart) < Epsilon) {
-                Console.WriteLine("OK");
+                Console.WriteLine("Ok");
             } else {
-                Console.WriteLine("Chyba: Očekávaná hodnota: {0}, Skutečná hodnota: {1}", c1, c2);
+                Console.WriteLine("Error: Expected value: {0}, Real value: {1}", c1, c2);
             }
         }
     }
