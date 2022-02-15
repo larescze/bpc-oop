ce3d4cd7f368b58ea9393e0ad6c85a5020180f05
Willi Lazarov <lazarov.w@gmail.com>
2022-02-15 09:57:46 +0100

    Add class for testing complex numbers

---
+++ TestComplex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv1
+{
+    public class TestComplex
+    {
+        public static void Test(Complex c1, Complex c2, string testName)
+        {         
+            const double Epsilon = 0.000001;
+
+            Console.Write(testName + ": ");
+
+            if (Math.Abs(c1.realPart - c2.realPart) < Epsilon && Math.Abs(c1.imaginaryPart - c2.imaginaryPart) < Epsilon) {
+                Console.WriteLine("OK");
+            } else {
+                Console.WriteLine("Chyba: Očekávaná hodnota: {0}, Skutečná hodnota: {1}", c1, c2);
+            }
+        }
+    }
+}
