f7ab5cb17a10d7562bb696923a85e239df37f8bb
Willi Lazarov <lazarov.w@gmail.com>
2022-02-22 08:46:51 +0100

    Remove duplicate matrix

--- Program.cs
+++ Program.cs
@@ -12,9 +12,8 @@ namespace cv3
         {
             Matrix m1 = new Matrix(new double[,] { { 3, 2, 7 }, { 2, 4, 0 } });
             Matrix m2 = new Matrix(new double[,] { { 1, 0, 0 }, { 0, 1, 2 } });
-            Matrix m3 = new Matrix(new double[,] { { 1, 0, 0 }, { 0, 1, 2 } });
-            Matrix m4 = new Matrix(new double[,] { { 1, -6, 2 }, { 1, 5, 0 }, { 3, 2, 3 } });
-            Matrix m5 = new Matrix(new double[,] { { 6, 1, 1 }, { 4, -2, 5 }, { 2, 8, 7 } });
+            Matrix m3 = new Matrix(new double[,] { { 1, -6, 2 }, { 1, 5, 0 }, { 3, 2, 3 } });
+            Matrix m4 = new Matrix(new double[,] { { 6, 1, 1 }, { 4, -2, 5 }, { 2, 8, 7 } });
 
             Console.WriteLine("Matrix (+):");
             Console.WriteLine(m1 + m2);
@@ -32,7 +31,7 @@ namespace cv3
             Console.WriteLine("Matrix (-u):");
             Console.WriteLine(-m1);
 
-            Console.WriteLine("Matrix determinant: {0}", m5.Determinant());
+            Console.WriteLine("Matrix determinant: {0}", m4.Determinant());
 
             Console.ReadLine();
         }
