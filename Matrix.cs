775c971538604a9e68983544e1a16a6c7548cd9f
Willi Lazarov <lazarov.w@gmail.com>
2022-02-22 12:40:17 +0100

    Improve exception throwing and add StringBuilder

--- Matrix.cs
+++ Matrix.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace cv3
 {
-    internal class Matrix
+    public class Matrix
     {
         private double[,] matrix;
-
+        private int Rows
+        {
+            get { return matrix.GetLength(0); }
+        }
+        public int Columns
+        {
+            get { return matrix.GetLength(1); }
+        }
+        
         public Matrix(double[,] matrix)
         {
             this.matrix = matrix;
@@ -17,242 +25,153 @@ namespace cv3
 
         public static Matrix operator +(Matrix m1, Matrix m2)
         {
-            try
+            if (m1.Rows == m2.Rows && m2.Columns == m2.Columns)
             {
-                if (m1.matrix.GetLength(0) == m2.matrix.GetLength(0) && m1.matrix.GetLength(1) == m2.matrix.GetLength(1))
-                {
-                    double[,] matrix = new double[m1.matrix.GetLength(0), m1.matrix.GetLength(1)];
+                double[,] matrix = new double[m1.Rows, m2.Columns];
 
-                    for (int i = 0; i < m1.matrix.GetLength(0); i++)
+                for (int row = 0; row < m1.Rows; row++)
+                {
+                    for (int column = 0; column < m2.Columns; column++)
                     {
-                        for (int j = 0; j < m1.matrix.GetLength(1); j++)
-                        {
-                            matrix[i, j] = m1.matrix[i, j] + m2.matrix[i, j];
-                        }
-                    }
-
-                    return new Matrix(matrix);
-                } 
-                else
-                {
-                    throw new ArgumentException("Matrix are not same size");
-                }
-            }
-            catch (ArgumentException e)
-            {
-                Console.Write("Matrix input error: {0}", e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);   
-            }
-
-            return null;
-        }
-
-        public static Matrix operator -(Matrix m1, Matrix m2)
-        {
-            try
-            {
-                if (m1.matrix.GetLength(0) == m2.matrix.GetLength(0) && m1.matrix.GetLength(1) == m2.matrix.GetLength(1))
-                {
-                    double[,] matrix = new double[m1.matrix.GetLength(0), m1.matrix.GetLength(1)];
-
-                    for (int i = 0; i < m1.matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < m1.matrix.GetLength(1); j++)
-                        {
-                            matrix[i, j] = m1.matrix[i, j] - m2.matrix[i, j];
-                        }
-                    }
-
-                    return new Matrix(matrix);
-                }
-                else
-                {
-                    throw new ArgumentException("Matrix are not same size");
-                }
-            }
-            catch (ArgumentException e)
-            {
-                Console.Write("Matrix input error: {0}", e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);
-            }
-
-            return null;
-        }
-
-        public static Matrix operator *(Matrix m1, Matrix m2)
-        {
-            try
-            {
-                if (m1.matrix.GetLength(1) == m2.matrix.GetLength(0))
-                {
-                    double[,] matrix = new double[m1.matrix.GetLength(0), m2.matrix.GetLength(1)];
-                    double temp;
-
-                    for (int i = 0; i < m1.matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < m2.matrix.GetLength(1); j++)
-                        {
-                            temp = 0;
-
-                            for (int k = 0; k < m1.matrix.GetLength(1); k++)
-                            {
-                                temp += m1.matrix[i, k] * m2.matrix[k, j];
-                            }
-
-                            matrix[i, j] = temp;
-                        }
-                    }
-
-                    return new Matrix(matrix);
-                }
-                else
-                {
-                    throw new ArgumentException("Matrix can't be multiplied");
-                } 
-            }
-            catch (ArgumentException e)
-            {
-                Console.Write("Matrix input error: {0}", e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);                
-            }
-
-            return null;
-        }
-
-        public static bool operator ==(Matrix m1, Matrix m2)
-        {
-            try
-            {
-                for (int i = 0; i < m1.matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < m1.matrix.GetLength(1); j++)
-                    {
-                        if (m1.matrix[i, j] != m2.matrix[i, j])
-                        {
-                            return false;
-                        }
-                    }
-                }
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);
-            }
-
-            return false;
-        }
-
-        public static bool operator !=(Matrix m1, Matrix m2)
-        {
-            try
-            {
-                return !(m1 == m2);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);
-            }
-
-            return false;
-        }
-
-        public static Matrix operator -(Matrix m)
-        {
-            try
-            {
-                double[,] matrix = new double[m.matrix.GetLength(0), m.matrix.GetLength(1)];
-
-                for (int i = 0; i < m.matrix.GetLength(1); i++)
-                {
-                    for (int j = 0; j < m.matrix.GetLength(0); j++)
-                    {
-                        matrix[j, i] = m.matrix[j, i] * (-1);
+                        matrix[row, column] = m1.matrix[row, column] + m2.matrix[row, column];
                     }
                 }
 
                 return new Matrix(matrix);
             }
-            catch (Exception e)
+
+            throw new ArgumentException("Matrix are not same size");
+        }
+
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            if (m1.Rows == m2.Rows && m2.Columns == m2.Columns)
             {
-                Console.WriteLine("Error: {0}", e.Message);
+                double[,] matrix = new double[m1.Rows, m2.Columns];
+
+                for (int row = 0; row < m1.Rows; row++)
+                {
+                    for (int column = 0; column < m2.Columns; column++)
+                    {
+                        matrix[row, column] = m1.matrix[row, column] - m2.matrix[row, column];
+                    }
+                }
+
+                return new Matrix(matrix);
             }
 
-            return null;
+            throw new ArgumentException("Matrix are not same size");
+        }
+
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if (m2.Columns == m2.Rows)
+            {
+                double[,] matrix = new double[m1.Rows, m2.Columns];
+                double temp;
+
+                for (int row = 0; row < m1.Rows; row++)
+                {
+                    for (int column = 0; column < m2.Columns; column++)
+                    {
+                        temp = 0;
+
+                        for (int i = 0; i < m2.Columns; i++)
+                        {
+                            temp += m1.matrix[row, i] * m2.matrix[i, column];
+                        }
+
+                        matrix[row, column] = temp;
+                    }
+                }
+
+                return new Matrix(matrix);
+            }
+
+            throw new ArgumentException("Matrix can't be multiplied");
+        }
+
+        public static bool operator ==(Matrix m1, Matrix m2)
+        {
+            for (int row = 0; row < m1.Rows; row++)
+            {
+                for (int column = 0; column < m2.Columns; column++)
+                {
+                    if (m1.matrix[row, column] != m2.matrix[row, column])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(Matrix m1, Matrix m2)
+        {
+            return !(m1 == m2);
+        }
+
+        public static Matrix operator -(Matrix m)
+        {
+            double[,] matrix = new double[m.Rows, m.Columns];
+
+            for (int row = 0; row < m.Rows; row++)
+            {
+                for (int column = 0; column < m.Columns; column++)
+                {
+                    matrix[row, column] = m.matrix[row, column] * (-1);
+                }
+            }
+
+            return new Matrix(matrix);
         }
 
         public double Determinant()
         {
-            try
+            if (this.Rows == this.Columns)
             {
-                if (this.matrix.GetLength(0) == this.matrix.GetLength(1)) 
-                { 
-                    if (this.matrix.GetLength(0) == 1)
-                    {
-                        return this.matrix[0, 0];
-                    }
-                    else if (this.matrix.GetLength(0) == 2)
-                    {
-                           return this.matrix[0, 0] * this.matrix[1, 1] - this.matrix[0, 1] * this.matrix[1, 0];
-                    }
-                    else if (this.matrix.GetLength(1) == 3)
-                    {
-                        return this.matrix[0, 0] * (this.matrix[1, 1] * this.matrix[2, 2] - this.matrix[1, 2] * this.matrix[2, 1])
-                             - this.matrix[0, 1] * (this.matrix[1, 0] * this.matrix[2, 2] - this.matrix[1, 2] * this.matrix[2, 0])
-                             + this.matrix[0, 2] * (this.matrix[1, 0] * this.matrix[2, 1] - this.matrix[1, 1] * this.matrix[2, 0]);
-                    }
-                    else
-                    {
-                       throw new ArgumentException("The dimension of a matrix is greater than 3x3");
-                    }
-                } 
-                else
+                if (this.Rows == 1)
                 {
-                   throw new ArgumentException("Matrix is not square");
+                    return this.matrix[0, 0];
                 }
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine("Matrix input error: " + e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);
+                else if (this.Rows == 2)
+                {
+                    return this.matrix[0, 0] * this.matrix[1, 1] - this.matrix[0, 1] * this.matrix[1, 0];
+                }
+                else if (this.Columns == 3)
+                {
+                    return this.matrix[0, 0] * (this.matrix[1, 1] * this.matrix[2, 2] - this.matrix[1, 2] * this.matrix[2, 1])
+                         - this.matrix[0, 1] * (this.matrix[1, 0] * this.matrix[2, 2] - this.matrix[1, 2] * this.matrix[2, 0])
+                         + this.matrix[0, 2] * (this.matrix[1, 0] * this.matrix[2, 1] - this.matrix[1, 1] * this.matrix[2, 0]);
+                }
+
+                throw new ArgumentException("The dimension of a matrix is greater than 3x3");
             }
 
-            return 0;
+            throw new ArgumentException("Matrix is not square");
         }
 
         public override string ToString()
         {
-            String matrix = "";
+            StringBuilder matrix =  new StringBuilder();
 
-            for (int i = 0; i < this.matrix.GetLength(0); i++)
+            for (int row = 0; row < this.Rows; row++)
             {
-                for (int j = 0; j < this.matrix.GetLength(1); j++)
+                for (int column = 0; column < this.Columns; column++)
                 {
-                    if (this.matrix[i, j] >= 0) 
-                    { 
-                        matrix += " ";    
+                    if (this.matrix[row, column] >= 0)
+                    {
+                        matrix.Append(" ");
                     }
 
-                    matrix += this.matrix[i, j] + " ";
+                    matrix.Append(this.matrix[row, column] + " ");
                 }
 
-                matrix += "\n";
+                matrix.AppendLine();
             }
 
-            return matrix;
+            return matrix.ToString();
         }
     }
 }
